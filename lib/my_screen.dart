a0f78d2da94ccb34df8156a0fcc00bd86fb5669f
shrpereira <silvioper@gmail.com>
2020-02-05 23:40:45 -0300

    Simplifying the Screen code

--- lib/my_screen.dart
+++ lib/my_screen.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:nuvigator/nuvigator.dart';
-import 'package:nuvigator_flutter_app/router.dart';
 
 class MyScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      builder: Nuvigator(
-        router: MyRouter(),
-        screenType: materialScreenType,
-        initialRoute: MyRoutes.myRoute,
-      ),
-    );
+    return Container();
   }
 }
